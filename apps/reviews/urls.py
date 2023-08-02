de11656af11a30ff031286b19ecdc9b66b0dc8a3
lyntchang <irenedielynsha@yahoo.com>
2023-08-02 11:50:19 -0400

    completed backend

---
+++ apps/reviews/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.ReviewList.as_view(), name='review_list'),
+    path('add', views.ReviewAdd.as_view(), name='review_add'),
+]
+
